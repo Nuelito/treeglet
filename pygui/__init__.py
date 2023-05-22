3e066ab426d3eb8f4deefc7661f0ffee8056683a
onuelito <90167681+onuelito@users.noreply.github.com>
2023-05-21 20:48:18 -0400

    purge

--- pygui/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tags import*
-from .gui import Gui
-from .widgets import*
-from .containers import Frame
-from .handler import MouseHandler
--- pygui/widgets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .image import Image
-from .label import Label
-from .button import Button
-from .entry import TextEntry
